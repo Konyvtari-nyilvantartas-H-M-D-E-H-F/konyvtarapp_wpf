8b02029846c93584675d82bed79ed486e552369f
Horvát Ferenc <bsze2horfer@vasvari.org>
2024-01-06 19:30:20 +0100

    hibajavitas

--- Books.Backend/obj/Debug/net7.0/Books.Backend.AssemblyInfo.cs
+++ Books.Backend/obj/Debug/net7.0/Books.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Books.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf2d67bf76f2208ed2518f61a3b90b348c62af89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Books.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Books.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KretaBasicSchoolSystem.Desktop/obj/Debug/net7.0-windows/konyvtarMVVM.AssemblyInfo.cs
+++ KretaBasicSchoolSystem.Desktop/obj/Debug/net7.0-windows/konyvtarMVVM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("konyvtarMVVM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d95331408d82d4a790a55a808e97ba8e4a0104")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("konyvtarMVVM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("konyvtarMVVM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
