c8f3155f0a236a97081ce685d3cc8502c1feedb3
hebmat <bsze2hebmat@vasvari.org>
2024-01-06 12:07:51 +0100

    majdnemkesz

--- Books.Backend/obj/Debug/net7.0/Books.Backend.AssemblyInfo.cs
+++ Books.Backend/obj/Debug/net7.0/Books.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Books.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d31584c89404b554bd78222f183575112f043b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d371a739d921b5f8f9b9cff5a4494309d292e624")]
 [assembly: System.Reflection.AssemblyProductAttribute("Books.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Books.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
